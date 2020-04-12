582d2289fd213a17975a3e4bed145c6d7865b082
ppssphyscis <psaouter@icloud.com>
2020-04-12 20:38:29 +0200

    finalize all library calls

--- R/libraries.R
+++ R/libraries.R
@@ -48,15 +48,15 @@ list_libraries <- function() {
 #' \dontrun{
 #'  list_library_blocks("UR_0NXtl92JJWqfWcJ")
 #' }
-#' @return A \code{list}.
+#' @return A tibble
 #' @export
 list_library_blocks <- function(library_id) {
 
-  .build_library_blocks <- function(lst) {
+  .build_df <- function(lst) {
     
     lst_names <- do.call(rbind,lapply(lst, function(x) tibble(directory = names(x))))
 
-    i <- 1
+    i <- 0
 
     do.call(
       bind_rows,
@@ -65,13 +65,14 @@ list_library_blocks <- function(library_id) {
         1,
         function(x) {
           
+          i <<- i+1
+
           dplyr::tibble(
             "dir_name" = as.character(x),
             "library_block_id" = names(lst[[i]][[as.character(x)]]),
             "library_block_name" = unlist(lst[[i]][[as.character(x)]])
           )
 
-          i <<- i+1
         }
     ))
     
@@ -82,12 +83,12 @@ list_library_blocks <- function(library_id) {
   getcnt <- .qualtrics_get(params, NULL, "offset" = offset)
 
   if (length(getcnt$result)>0) {
-    df <- .build_library_blocks(getcnt$result$elements)
+    df <- .build_df(getcnt$result$elements)
   
     while (!is.null(getcnt$result$nextPage)) {
       offset <- httr::parse_url(getcnt$result$nextPage)$query$offset
       getcnt <- .qualtrics_get(params, NULL, "offset" = offset)
-      df <- rbind(df,.build_library_blocks(getcnt$result$elements))
+      df <- rbind(df, .build_df(getcnt$result$elements))
     }
     return(df)
   } else {
@@ -95,3 +96,119 @@ list_library_blocks <- function(library_id) {
   }
 
 }
+
+#' Lists the library questions for the caller of this API.
+#'
+#' @param library_id the library id (see 'list_libraries')
+#' 
+#' @example
+#' \dontrun{
+#'  list_library_questions("UR_0NXtl92JJWqfWcJ")
+#' }
+#' @return A tibble
+#' @export
+list_library_questions <- function(library_id) {
+
+  .build_df <- function(lst) {
+    
+    lst_names <- do.call(rbind,lapply(lst, function(x) tibble(directory = names(x))))
+
+    i <- 0
+
+    do.call(
+      bind_rows,
+      apply(
+        lst_names,
+        1,
+        function(x) {
+          
+          i <<- i+1
+
+          dplyr::tibble(
+            "dir_name" = as.character(x),
+            "library_question_id" = names(lst[[i]][[as.character(x)]]),
+            "library_question_name" = unlist(lst[[i]][[as.character(x)]])
+          )
+
+        }
+    ))
+    
+    }
+
+  offset <- 0
+  params <- c("libraries", library_id, "survey", "questions")
+  getcnt <- .qualtrics_get(params, NULL, "offset" = offset)
+
+  if (length(getcnt$result)>0) {
+    df <- .build_df(getcnt$result$elements)
+  
+    while (!is.null(getcnt$result$nextPage)) {
+      offset <- httr::parse_url(getcnt$result$nextPage)$query$offset
+      getcnt <- .qualtrics_get(params, NULL, "offset" = offset)
+      df <- rbind(df,.build_df(getcnt$result$elements))
+    }
+    return(df)
+  } else {
+    return(NULL)
+  }
+
+}
+
+#' Lists the library surveys for the caller of this API.
+#'
+#' @param library_id the library id (see 'list_libraries')
+#' 
+#' @example
+#' \dontrun{
+#'  list_library_surveys("UR_0NXtl92JJWqfWcJ")
+#' }
+#' @return A tibble
+#' @export
+list_library_surveys <- function(library_id) {
+
+  .build_df <- function(lst) {
+    
+    lst_names <- do.call(rbind,lapply(lst, function(x) tibble(directory = names(x))))
+
+    i <- 0
+
+    do.call(
+      bind_rows,
+      apply(
+        lst_names,
+        1,
+        function(x) {
+          
+          i <<- i+1
+
+          dplyr::tibble(
+            "dir_name" = as.character(x),
+            "library_survey_id" = names(lst[[i]][[as.character(x)]]),
+            "library_survey_name" = unlist(lst[[i]][[as.character(x)]])
+          )
+
+        }
+    ))
+    
+    }
+
+  offset <- 0
+  params <- c("libraries", library_id, "survey", "surveys")
+  getcnt <- .qualtrics_get(params, NULL, "offset" = offset)
+
+  if (length(getcnt$result)>0) {
+    df <- .build_df(getcnt$result$elements)
+  
+    while (!is.null(getcnt$result$nextPage)) {
+      offset <- httr::parse_url(getcnt$result$nextPage)$query$offset
+      getcnt <- .qualtrics_get(params, NULL, "offset" = offset)
+      df <- rbind(df,.build_df(getcnt$result$elements))
+    }
+    return(df)
+  } else {
+    return(NULL)
+  }
+
+}
+
+
